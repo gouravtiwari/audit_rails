8bb91f21dd81cb52b3970331319a09f437750fd0
Gourav Tiwari <gouravtiwari21@gmail.com>
2012-12-17 19:41:58 -0500

    Updated audit model with corrections

--- app/models/audit_rails/audit.rb
+++ app/models/audit_rails/audit.rb
@@ -3,7 +3,7 @@ module AuditRails
     attr_accessible :action, :controller, :description, :user_name
 
     def self.no_audit_entry_for_today?(action_name, user_name)
-      audits = Audit.where(action: action_name, user_name: user_name, 
+      audits = AuditRails::Audit.where(action: action_name, user_name: user_name, 
         created_at: Time.now.to_date.beginning_of_day..Time.now.to_date.end_of_day)
       audits.blank?
     end
