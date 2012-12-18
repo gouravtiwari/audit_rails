7d859d5255898092c688a65351eff62a37f7424a
Gourav Tiwari <gouravtiwari21@gmail.com>
2012-12-17 19:44:13 -0500

    moved add_to_audit to application controller, so that it is available for all controllers

--- app/controllers/audit_rails/application_controller.rb
+++ app/controllers/audit_rails/application_controller.rb
@@ -1,4 +1,20 @@
 module AuditRails
   class ApplicationController < ActionController::Base
+
+  	helper_method :add_to_audit
+  	
+  	def add_to_audit(action_name=nil, controller_name=nil, user_name=nil, description=nil)
+      if action_name == "login"
+        if AuditRails::Audit.no_audit_entry?(action_name, user_name)
+          AuditRails::Audit.create(action: action_name, controller: controller_name, user_name: user_name, description: description)
+        end
+      else
+        AuditRails::Audit.create(action: action_name || request.params[:action],
+          controller: controller_name || request.params[:controller],
+          user_name: user_name, 
+          description: description)
+      end
+    end
+
   end
 end
--- app/controllers/audit_rails/audits_controller.rb
+++ app/controllers/audit_rails/audits_controller.rb
@@ -2,10 +2,10 @@ require_dependency "audit_rails/application_controller"
 
 module AuditRails
   class AuditsController < ApplicationController
-    before_filter(:only => :create) {|c| c.add_to_audit("visit-site", "xyz", "Fake User")}
+    before_filter(:only => :create) {|c| add_to_audit("visit-site", "xyz", "Fake User")}
 
     def index
-      @audits = Audit.all
+      @audits = AuditRails::Audit.all
   
       respond_to do |format|
         format.html # index.html.erb
@@ -15,18 +15,6 @@ module AuditRails
 
     def create
       render :nothing => true, :status => 200, :content_type => 'text/html'
-    end
-    
-    def add_to_audit(action_name=nil, controller_name=nil, user_name=nil)
-      if action_name == "login"
-        if Audit.no_audit_entry?(action_name, user_name)
-        Audit.create(action: action_name, controller: controller_name, user_name: user_name)
-        end
-      else
-        Audit.create(action: action_name || request.params[:action],
-        controller: controller_name || request.params[:controller],
-        user_name: user_name)
-      end
     end
 
   end
