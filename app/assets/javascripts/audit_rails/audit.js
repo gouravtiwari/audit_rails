123263bde3aeed13ec0c6137ea50375373856569
Gourav Tiwari <gouravtiwari21@gmail.com>
2013-06-28 00:59:45 -0400

    Updated sample JS function to use on() as live() is deprecated

--- app/assets/javascripts/audit_rails/audit.js
+++ app/assets/javascripts/audit_rails/audit.js
@@ -1,4 +1,4 @@
-$("a.visit-site").live("click", function() {
+$(document).on("click", 'a.visit-site', function() {
 	params = { audit: { url: this.href, app_id: $("a.visit-site").attr("data-app-id") } };
 	$.post("/audit_rails/audits", params);
 	window.open(this.href);
