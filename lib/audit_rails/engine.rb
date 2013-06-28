156d21caf3668ae61aa1275056ee38c2b3e21998
Gourav Tiwari <gouravtiwari21@gmail.com>
2013-06-28 00:17:18 -0400

    moved add_to_audit method to helper as it allows more flexibility to be added to application's controllers

--- lib/audit_rails/engine.rb
+++ lib/audit_rails/engine.rb
@@ -4,6 +4,10 @@ require 'to_xls'
 module AuditRails
   class Engine < ::Rails::Engine
     isolate_namespace AuditRails
+
+    initializer "audit_rails.load_helpers" do |app|
+      ActionController::Base.send :include, AuditRails::AuditsHelper
+    end
     
     config.generators do |g|
       g.test_framework      :rspec
