b94d390521428646d0f4db7a285080653ddfe32a
Gourav Tiwari <gouravtiwari21@gmail.com>
2013-11-24 02:13:27 +0530

    fixed controller spec

--- spec/controllers/audit_rails/audits_controller_spec.rb
+++ spec/controllers/audit_rails/audits_controller_spec.rb
@@ -70,7 +70,12 @@ describe AuditRails::AuditsController do
       # list should be a hash
       AuditRails::Audit.stub(:in_range).and_return(stub(count: count = 9,
         analysis_by_user_name: user_list = {"Fake User"=>6, "John Smith"=>3},
-        analysis_by_page_views: page_list = {"visit-site"=>6, "login"=>3}
+        analysis_by_page_views: page_list = {"visit-site"=>6, "login"=>3},
+        analysis_per_user_by_page_views: users_by_page_list = {"Fake User"=> [{"page"=>"visit-site", "count"=>4},
+                                                                              {"page"=>"login", "count"=>2}], 
+                                                                "John Smith" => [{"page"=>"visit-site", "count"=>2},
+                                                                              {"page"=>"login", "count"=>1}]
+                                                              }
         ))
 
       get 'analytics'
@@ -78,6 +83,7 @@ describe AuditRails::AuditsController do
       expect(assigns(:total)).to eq(count)
       expect(assigns(:analysis_by_user_name)).to eq(user_list)
       expect(assigns(:analysis_by_page_views)).to eq(page_list)
+      expect(assigns(:analysis_per_user_by_page_views)).to eq(users_by_page_list)
     end
   end
 end
