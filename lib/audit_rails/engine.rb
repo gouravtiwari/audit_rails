6c1217b8edf9507f821407eb64bd140851137926
Gourav Tiwari <gouravtiwari21@gmail.com>
2013-12-08 02:51:16 +0530

    added asset paths for sleek_rails assets :smile:
    Loved this thought:
    "Rails also doesn’t automatically add the asset paths for Sprockets, so
    if your outer engine wants to use assets from an inner engine, you’ll
    need to add those paths yourself"
    http://developer.zendesk.com/blog/2012/03/01/engines-lessons-learned/

--- lib/audit_rails/engine.rb
+++ lib/audit_rails/engine.rb
@@ -5,6 +5,13 @@ module AuditRails
   class Engine < ::Rails::Engine
     isolate_namespace AuditRails
 
+    initializer :append_dependent_assets_path, :group => :all do |app| 
+      require 'sleek_charts'
+      engine_root = SleekCharts::Engine.root
+      app.config.assets.paths.push(engine_root + 'app/assets/javascripts')
+      app.config.assets.paths.push(engine_root + 'app/assets/stylesheets')
+    end
+
     initializer "audit_rails.load_helpers" do |app|
       ActionController::Base.send :include, AuditRails::AuditsHelper
     end
