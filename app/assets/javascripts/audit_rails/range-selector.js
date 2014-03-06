ce684de943188964b6df8c1a7ddc3ac73a3ffbc7
Gourav Tiwari <gouravtiwari21@gmail.com>
2014-03-07 02:12:09 +0530

    date range update on range-selection :dizzy_face:

--- app/assets/javascripts/audit_rails/range-selector.js
+++ app/assets/javascripts/audit_rails/range-selector.js
@@ -5,22 +5,26 @@ function data_temp(elementId) {
 
   for(var dateStr in counts){
     if(dateStr == 'Date'){
-      head = dateStr + ',' + counts[dateStr] + '\n';
+      head = head + dateStr + ',' + counts[dateStr] + '\n';
     }else{
-      str = str + dateStr + ',' + counts[dateStr] + ';' + counts[dateStr] + ';' + counts[dateStr] + '\n';
+      str = str + dateStr + ',' + counts[dateStr] + '\n';
     }
   }
 
   return head + str;
 }
 
+function updateDateRange(startDate, endDate){
+  $('#analytics_range_begin').val(startDate);
+  $('#analytics_range_end').val(endDate);
+}
+
 g1 = new Dygraph(
     document.getElementById("noroll"),
     data_temp("noroll"),
     {
-      customBars: true,
-      height: 320,
-      title: 'Page Views',
+      height: 150,
+      title: '',
       ylabel: '',
       // showLabelsOnHighlight: false,
       // drawYAxis: false,
@@ -29,10 +33,17 @@ g1 = new Dygraph(
       drawXGrid: false,
       drawYGrid: false,
       showRangeSelector: true,
-      rangeSelectorHeight: 70,
+      rangeSelectorHeight: 50,
       rangeSelectorPlotFillColor: "#e98e4e",
       rangeSelectorPlotStrokeColor: "#e98e4e",
       showRoller: false,
-      axisLineColor: '#f2f2f2'
+      axisLineColor: '#f2f2f2',
+      drawCallback: function(g) {
+        if (g.dateWindow_ != undefined) {
+          var startDate = $.datepicker.formatDate('yy-mm-dd',new Date(g.dateWindow_[0]));
+          var endDate = $.datepicker.formatDate('yy-mm-dd',new Date(g.dateWindow_[1]));
+          updateDateRange(startDate, endDate);
+        }
+      }
     }
 );
